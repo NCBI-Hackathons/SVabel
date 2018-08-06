35d5cfa37985c538d3801348219c1b57a3e6a46d
Evan Biederstedt <evan.biederstedt@gmail.com>
2018-08-06 15:52:35 -0400

    added package

---
+++ R/functions.R
@@ -0,0 +1,22 @@
+
+
+#' @name addition
+#' @title outputs the sum of two arguments
+#' @description
+#'
+#' outputs the sum of two arguments
+#' the inputs should be numeric
+#'
+#' @param arg1 numeric first argument
+#' @param arg2 numeric second argument
+#' @return numeric Sum 
+#' @export
+addition = function(arg1, arg2){
+    if(!is(arg1, 'numeric')){
+    	stop("arg1 is not numeric")
+    }
+    if (!is(arg2, 'numeric')){
+    	stop("arg2 is not numeric")
+    }
+    return(arg1+arg2)
+}
