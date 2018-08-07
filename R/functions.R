e383a158eac4dab5751dea251ed6906cf6184934
programmingbiochem <41163764+programmingbiochem@users.noreply.github.com>
2018-08-07 12:07:55 -0400

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -1,23 +1,9 @@
 
-#' @import data.table
-
-#' @name addition
-#' @title outputs the sum of two arguments
-#' @description
+#' HG-19 intron data
 #'
-#' outputs the sum of two arguments
-#' the inputs should be numeric
-#'
-#' @param arg1 numeric first argument
-#' @param arg2 numeric second argument
-#' @return numeric Sum 
-#' @export
-addition = function(arg1, arg2){
-    if(!is(arg1, 'numeric')){
-    	stop("arg1 is not numeric")
-    }
-    if (!is(arg2, 'numeric')){
-    	stop("arg2 is not numeric")
-    }
-    return(arg1+arg2)
-}
+#' Downloaded from Fusion VizR zip file
+#' @name introns_as_GRanges
+#' @docType data
+#' @keywords data
+#' @format \code{GRanges}
+NULL
