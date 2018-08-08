51b30a9e82e454cb3c86da963377c39b04527235
programmingbiochem <41163764+programmingbiochem@users.noreply.github.com>
2018-08-08 10:57:54 -0400

    Updated code to read in VCF file and ask user for input

--- R/Reading in VCF file.R
+++ R/Reading in VCF file.R
@@ -1,30 +1,50 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite("VariantAnnotation")
 library(VariantAnnotation)
+install.packages("stringr")
+library(stringr)
+install.packages("stringi")
+library(stringi)
 
-# Ask user for path and name of file
-readfile <- function()
-  
+readfile <- function() 
 {
-  filename <- readline(prompt = "Enter path and name of VCF file.")
-  return(filename)
+  # Ask user for name of VCF file
+  filename<-readline(prompt="Enter path and name of .VCF or .VCF.GZ file.")
+  # If the user enters the path with quotes, then remove quotes and the backslash that R adds
+  filename<-gsub('\"', "", filename, fixed = TRUE)
   
-  vcf_file <- readVcf(filename, genome="hg19")
-  vcf <- rowRanges(vcf_file)
-}
+  # Make sure that the user enters the correct file. Do not proceed unless file is .VCF or .VCF.GZ
+  while (file.exists(filename)==FALSE)
+  {
+   print("File does not exist. Check the path and name of the file, including extension.")
+     filename<-readline(prompt="Re-enter path and name of file. File must be .VCF or .VCF.GZ.")
+     filename<-gsub('\"', "", filename, fixed = TRUE)
+   }
+
+  # Ask user if he/she is using hg19 data
+  ans<-readline(prompt="Are you using hg19? y/n.")
+
+   # Make sure user enters correct value
+   while (toupper(ans) !="Y" && toupper(ans) !="N")
+   {
+     ans<-readline(prompt="Enter 'y' or 'n'.")
+   }
+  
+   # Create value for gen parameter based on user input
+   if (toupper(ans) == "Y") {
+     gen="hg19"
+   }
+  
+   else if (toupper(ans) == "N") {
+     gen="hg38"
+   }
+  
+   # Read in .VCF or .VCF.GZ file
+   vcf_file <- readVcf(filename, genome=gen)
+  
+   # Convert S4 data object to data frame
+   vcf <- rowRanges(vcf_file)
+  }
 
 readfile()
 
-
-# Read in the VCF file
-
-
-# Convert to data frame
-
-
-head(vcf)
-keeps <- c("QUAL","ALT","ParamRangeID")
-vcf[keeps]
-
-library(GenomicRanges)
-example_foo = GRanges('12:500-760')
